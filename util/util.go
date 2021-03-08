63ae09f45a6bae40aab03c5535cfa21ae3026b61
David A. Lareo <dalareo@gmail.com>
2021-03-08 17:33:09 +0100

    solved Golang hexutil obsolete library

--- util/util.go
+++ util/util.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/common/math"
 )
 
@@ -27,7 +28,7 @@ func MakeTargetHex(minerDifficulty float64) string {
 	minerAdjustedDifficulty := int64(minerDifficulty * 1000000 * 100)
 	difficulty := big.NewInt(minerAdjustedDifficulty)
 	diff1 := new(big.Int).Div(pow256, difficulty)
-	return string(common.ToHex(diff1.Bytes()))
+	return string(hexutil.Encode(diff1.Bytes()))
 }
 
 func TargetHexToDiff(targetHex string) *big.Int {
